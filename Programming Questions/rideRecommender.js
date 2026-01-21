6ebfd6b3bf7203e7593b38d9d85eee7a550c483f
HyPhamCS2043 <giahy.baohan@gmail.com>
2026-01-21 16:11:40 -0400

    Added if statement to check time and month. Added if statement to print out appropriate messages if park is closed or open. Clean up unused variables and comments

--- Programming Questions/rideRecommender.js
+++ Programming Questions/rideRecommender.js
@@ -1,33 +1,12 @@
 //Author: Hy Pham
 //3644179
 
-function parkRideRequirements (age, height, weight, mood, view) {
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-    this.mood = mood;
-    this.view = view;
-}
-
-//const ferrisWheel = parkRideRequirements(10, 152.4, 0, "calm", true);
-//const rollerCoaster = parkRideRequirements(10, 152.4, 0, "exciting", true);
-//const bumperCars = parkRideRequirements(14, 152.4, 36.29, "exciting", null);
-//const merryGoRound = parkRideRequirements(4, 0, 0, "calm", null); //recommended for under 12 too
-//const tiltAWhirl = parkRideRequirements(4, 152.4, 36.29, "exciting", null);
-
 
 const ferrisWheel = [10, 152.4, 0, "Calm", true];
 const rollerCoaster = [10, 152.4, 0, "Exciting", true];
 const bumperCars = [14, 152.4, 36.29, "Exciting", false];
 const merryGoRound = [4, 0, 0, "Calm", false]; //only recommended if under 12 
 const tiltAWhirl = [4, 152.4, 36.29, "Exciting", false];
-
-const openningHour = new Date("Jan 1, 2026 09:00:00");
-const closingHour = new Date("April 30, 2026 21:00:00")
-
-//Get customer's arrival time (system time)
-var customerTime = new Date();
-console.log(`Your arrival time: ${customerTime.getHours()}:${customerTime.getMinutes()} ` );
 
 
 //Function that compares the customer request with a ride's requirements.
@@ -56,6 +35,7 @@ const prompt = require('prompt-sync')({sigint: true});
 
 console.log("In 1 line, enter your age, height, weight, preferred ride mood, and Yes if you want a view, N/A if not:\n");
 let customerInput = prompt();
+console.log(); //Add a line between user input and output
 
 //Split customer input into an array using "," as delimiter.
 var criteria = customerInput.split(",");
@@ -64,7 +44,36 @@ if (criteria.length < 5) {
     throw new Error("Not enough information, please input 5 clauses separated by comma.")
 }
 
-console.log("The input is: " + customerInput);
+//console.log("The input is: " + customerInput);
+
+//Get customer's arrival time (system time)
+var customerTime = new Date();
+customerTime.setMinutes(59);
+
+//If current time is at exactly 00 minutes, format output string to print 00 instead of 0
+var currentMinutes = customerTime.getMinutes() === 0 ? "00" : customerTime.getMinutes();
+
+console.log(`Current time: ${customerTime.getHours()}:${currentMinutes} ` );
+
+//Variable to indicate whether the park is open or not
+var isOpen = true;
+
+//If the customer does not arrive between January (which is 0)
+//and April (which is 3)
+if (customerTime.getMonth() > 3) {
+    isOpen = false;
+}
+
+//Check if the visitor arrives between 11 AM and 11 PM (a.k.a. 23:00)
+if (customerTime.getHours() < 11 || customerTime.getHours() >= 23) {
+    isOpen = false;
+}
+
+if(isOpen) {
+    console.log("The Amusement Park is OPEN.");
+} else {
+    console.log("The Amusement Park is CLOSED.");
+}
 
 //Clean up the data by removing white spaces.
 for(i = 0; i < 5; i++) {
@@ -72,60 +81,60 @@ for(i = 0; i < 5; i++) {
     //console.log(criteria[i]);
 }
 
+console.log("Ride Recommendations:");
+
 //Taking care of clear edge cases, such as when visitors are younger than 4
 if(criteria[0] < 4) { 
-    console.log("There are currently no rides in the park that are suitable.");
+    console.log("\tThere are currently no rides in the park that are suitable.");
 } else { //Visitors are older than 4
     //Visitors are shorter than 152.4 cm
     if(criteria[1] < 152.4) {
         //Only recommend if visitor is younger than 12
         if(criteria[0] < 12) {
             if (checkRequirements(criteria, merryGoRound)) {
-                console.log("Merry-Go-Round");
+                console.log("\tMerry-Go-Round");
             } else {
-                console.log("There are currently no rides in the park that are suitable.");
+                console.log("\tThere are currently no rides in the park that are suitable.");
             }
 
+        } else {
+           console.log("\tThere are currently no rides in the park that are suitable."); 
         }
-    //Visitors are taller than 152.4
-    } else { 
 
-        /* if (criteria[2] > 36.29 ) {
-            if (checkRequirements(criteria, bumperCars)) {
-                console.log("Bumper Cars");
-            }
-            if (checkRequirements(criteria, tiltAWhirl)) {
-                console.log("Tilt-A-Whirl");
-            } 
-        } */
-
+    } else { //Visitors are taller than 152.4
+        //Variable to check if any rides are recommended
         var hasARide = false;
         
         if (checkRequirements(criteria, bumperCars)) {
-            console.log("Bumper Cars");
+            console.log("\tBumper Cars");
             hasARide = true;
         }
         if (checkRequirements(criteria, ferrisWheel)) {
-            console.log("Ferris Wheel");
+            console.log("\tFerris Wheel");
             hasARide = true;
         }
         if (checkRequirements(criteria, merryGoRound)) {
-            console.log("Merry-Go-Round");
+            console.log("\tMerry-Go-Round");
             hasARide = true;
         } 
         if (checkRequirements(criteria, rollerCoaster)) {
-            console.log("Roller Coaster");
+            console.log("\tRoller Coaster");
             hasARide = true;
         } 
         if (checkRequirements(criteria, tiltAWhirl)) {
-            console.log("Tilt-A-Whirl");
+            console.log("\tTilt-A-Whirl");
             hasARide = true;
         } 
 
         if(!hasARide) {
-            console.log("There are currently no rides in the park that are suitable.")
+            console.log("\tThere are currently no rides in the park that are suitable.")
         }
 
     }
 }
 
+if(isOpen) {
+    console.log("Hope to see you soon!");
+} else {
+    console.log("We hope to see you when our park opens, every day 11AM-11PM from January 1st to April 30th.");
+}

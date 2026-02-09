14487a6db08f45c5c9e88b984103f275b424c1fd
HyPhamCS2043 <giahy.baohan@gmail.com>
2026-02-09 10:18:10 -0400

    finish function min and max

--- Programming Questions/mathFunctions.py
+++ Programming Questions/mathFunctions.py
@@ -1,16 +1,15 @@
 #Author: Hy Pham
 #Student number: 3644179
 
+
+testArr = [1.0, 2.0, 3.0, 4.0, 5.0]
 #Function to calculate the sum of all numbers in the input array
 def calSum(inputArr):
-    i = 0
     curSum = 0
-    while i < len(inputArr):
-        curSum += inputArr[i]
-        i += 1
+    for i in inputArr:
+        curSum += i
     return curSum
 
-testArr = [1, 2, 3, 4, 5]
 print(calSum(testArr))
 
 #Function to calculate the average of all numbers in the input array
@@ -19,3 +18,22 @@ def calAverage(inputArr):
     return total/len(inputArr)
 
 print(calAverage(testArr))
+
+#
+def calMax(inputArr):
+    curMax = inputArr[0]
+    for i in inputArr:
+        if i > curMax:
+            curMax = i
+    return curMax
+
+print(calMax(testArr))
+
+def calMin(inputArr):
+    curMin = inputArr[0]
+    for i in inputArr:
+        if i < curMin:
+            curMin = i
+    return curMin
+
+print(calMin(testArr))

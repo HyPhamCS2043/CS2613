dd46259ee1db0a59a9a6c57707f152c502f155a5
HyPhamCS2043 <giahy.baohan@gmail.com>
2026-03-16 12:17:11 -0300

    finished the remaining functions to calculate expo, poisson, sine, cosine

--- Programming Questions/mathFunctions.m
+++ Programming Questions/mathFunctions.m
@@ -2,7 +2,7 @@
 #Author: Hy Pham
 #3644179
 
-testMatrix = [1 2 3 4 5];
+testMatrix = [1.0 2.0 3.0 4.0 5.0];
 
 #Function to calculate the sum of all numbers in the input list
 function sumOut = calSum (lst)
@@ -70,6 +70,64 @@ disp("The value of calPower(2, 3) is:"), disp(calPower(2, 3));
 
 #Function that approximate exponential function for each
 #value in input list
-function lstOut = calExpo(lst)
-    
+function listOut = calExpo(lst)
+    listOut = [];
+    for i = lst
+        curCalResult = 0;
+        for k = [0:51]
+            curCalResult += calPower(i, k)/calFactorial(k);
+        endfor
+        listOut = [listOut; curCalResult];
+    endfor
 endfunction
+
+output_precision (7)
+calExpo(testMatrix)
+
+#Function that approximate the Poisson distribution for each
+#value in input list
+function listOut = calPoisson(lst)
+    listOut = [];
+    for i = lst
+        curCalResult = 0;
+        for k = [0:51]
+            curCalResult += k * (calPower(i, k)/calFactorial(k));
+        endfor
+        listOut = [listOut; curCalResult];
+    endfor
+endfunction
+
+calPoisson(testMatrix)
+
+#Function that approximate the Sin value for each
+#value in input list
+function listOut = calSin(lst)
+    listOut = [];
+    for i = lst
+        curCalResult = 0;
+        for k = [0:51]
+            key = 2 * k + 1;
+            curCalResult += (calPower(-1, k) * (calPower(i, key))/calFactorial(key));
+        endfor
+        listOut = [listOut; curCalResult];
+    endfor
+endfunction
+
+output_precision(5)
+calSin(testMatrix)
+
+#Function that approximate cosine value for each
+#value in input list
+function listOut = calCos(lst)
+    listOut = [];
+    for i = lst
+        curCalResult = 0;
+        for k = [0:51]
+            key = 2 * k;
+            curCalResult += (calPower(-1, k) * (calPower(i, key))/calFactorial(key));
+        endfor
+        listOut = [listOut; curCalResult];
+    endfor
+endfunction
+
+calCos(testMatrix)

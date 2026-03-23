08a44514911117b2c7bbf92c73b84289b8dbce14
HyPhamCS2043 <giahy.baohan@gmail.com>
2026-03-23 10:20:34 -0300

    create Node class, DoubleLinkedList class, and a method in DoubleLinkedList to add a Node to the end of the list

--- Programming Questions/StudentClassListOrganizer.py
+++ Programming Questions/StudentClassListOrganizer.py
@@ -1,3 +1,30 @@
 #Author: Hy Pham
-#Student ID: 3644179
+#Student ID: 364417
 
+#Node class for double linked list implementation
+class Node:
+    def __init__(self, data):
+        self.next = None
+        self.prev = None
+        self.data = data
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def insert_end(self, dataIn):
+        newNode = Node(dataIn)
+        if self.length == 0:
+            self.head = newNode
+            self.tail = newNode
+            
+        else:
+            newNode.prev = self.tail
+            self.tail.next = newNode
+            self.tail = newNode  
+
+        self.length += 1
+    

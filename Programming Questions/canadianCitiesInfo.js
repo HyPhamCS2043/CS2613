1981ad8463e4dae71ffe3385f8e3c632b580931b
HyPhamCS2043 <giahy.baohan@gmail.com>
2026-04-01 10:23:29 -0300

    Create API key var and remove mine. Add code section to retrieve the five closest cities based on user input radius

--- Programming Questions/canadianCitiesInfo.js
+++ Programming Questions/canadianCitiesInfo.js
@@ -1,6 +1,8 @@
 //Author: Hy Pham
 //Student number: 3644179
 
+//Add API Key here to access server
+const apiKey = '';
 
 const prompt = require('prompt-sync')({sigint: true});
 console.log(`Please enter, in one line, "[City], [Canadian Province/Territory]"`);
@@ -10,7 +12,8 @@ var searchParams = input.split(",");
 //console.log(`City: ${searchParams[0]}, Province = ${searchParams[1]}`);
 var urlToSearch = `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/CA/regions/${searchParams[1].trim()}/cities`;
 //console.log(`URL: ${urlToSearch}`)
-//Snippet partially from rapidapi.com
+
+//Snippet partially from rapidapi.com 
 const axios = require('axios');
 
 const options = {
@@ -21,7 +24,7 @@ const options = {
     limit: '1'
   },
   headers: {
-    'x-rapidapi-key': 'dd8e5c4c15msh69aff2ca5bb366ap17df84jsn94dfa240bd9a',
+    'x-rapidapi-key': apiKey,
     'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
     'Content-Type': 'application/json'
   }
@@ -42,17 +45,62 @@ async function fetchData() {
 
             var firstOutput = "";
             if (population > 0) {
-                firstOutput = `${searchParams[0]}, ${searchParams[1]} (${latitude}, ${longitude}) has a population of ${population}`;
+                firstOutput = `${searchParams[0]}, ${searchParams[1].trim()} (${latitude}, ${longitude}) has a population of ${population}`;
             }
             else {
-                firstOutput = `${searchParams[0]}, ${searchParams[1]} (${latitude}, ${longitude}) does not have population data`;
+                firstOutput = `${searchParams[0]}, ${searchParams[1].trim()} (${latitude}, ${longitude}) does not have population data`;
             }
             console.log(firstOutput);
+
+            //If city is found, prompt user for radius and send request to server
+            console.log("Input radius in kilometers:");
+            let radiusInput = prompt();
+
+            //Snippet partially from rapidapi.com
+            const cityOptions = {
+            method: 'GET',
+            url: `https://wft-geo-db.p.rapidapi.com/v1/geo/cities/${wikiDataId}/nearbyCities`,
+            params: {
+                radius: radiusInput,
+                distanceUnit: 'KM',
+                excludedCountryIds: 'Q30'
+            },
+            headers: {
+                'x-rapidapi-key': apiKey,
+                'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+                'Content-Type': 'application/json'
+            }
+            };
+
+            async function fetchCityData() {
+                try {
+                    const response = await axios.request(cityOptions);
+                    
+                    var numRetData = response.data.data.length;
+
+                    if (numRetData == 0) {
+                        console.log(`There is no cities within ${radiusInput} km of ${searchParams[0]}, ${searchParams[1].trim()}`);
+                    }
+                    else {
+                        console.log(`Some cities close to ${searchParams[0]}, ${searchParams[1].trim()} are:`);
+                        for (let i = 0; i < numRetData; i++) {
+                            var cityName = response.data.data[i].name;
+                            console.log(`\t${cityName}`);
+                        }
+                    }
+                    
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+
+            fetchCityData();
         }
         
 	} catch (error) {
 		console.error(error);
 	}
+
 }
 
 fetchData();
